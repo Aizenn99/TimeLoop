e9da30e97f78428db750ef9a4f5566a4b15a5dfc
het <kalriyahet@gmail.com>
2025-07-06 15:07:57 +0530

    some changes

--- src/Pages/Githubcontri.jsx
+++ src/Pages/Githubcontri.jsx
@@ -7,13 +7,18 @@ const Githubcontri = () => {
 
   return (
     <div className="w-screen h-screen flex items-center justify-center bg-background p-4">
-      <div className="p-4 rounded-xl w-full max-w-5xl flex flex-col font-mono items-center justify-center tracking-tighter border border-black text-white ">
-        <DatabaseWithRestApi />
+      <div className="p-4 rounded-xl w-full max-w-5xl flex flex-col font-mono items-center justify-center tracking-tighter border border-black  ">
+      <div className="text-white" >
+         <DatabaseWithRestApi />
+      </div>
+       
         <div>
           <h2 className="text-2xl font-bold mb-4 mt-6  text-black font-mono text-center">
             My GitHub Contributions (2025)
           </h2>
         </div>
+
+        <div className="text-black" >
 
         <GitHubCalendar
           username="Aizenn99"
@@ -25,6 +30,7 @@ const Githubcontri = () => {
             data.filter((day) => new Date(day.date) >= fromDate)
           }
         />
+        </div>
       </div>
     </div>
   );
